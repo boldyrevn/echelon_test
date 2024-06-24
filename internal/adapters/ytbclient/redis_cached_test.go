a30211983407a0df259bd62e70bcd8a33c75e7b0
boldyrevn <boldyrev.now@mail.ru>
2024-06-24 14:09:45 +0300

    fix redis test

--- internal/adapters/ytbclient/redis_cached_test.go
+++ internal/adapters/ytbclient/redis_cached_test.go
@@ -1,51 +1,46 @@
 package ytbclient
 
 import (
-	"context"
-	"echelon_task/internal/model"
-	"fmt"
-	"os"
-	"testing"
+    "context"
+    "echelon_task/internal/model"
+    "fmt"
+    "os"
+    "testing"
 
-	"github.com/redis/go-redis/v9"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"go.uber.org/mock/gomock"
+    "github.com/redis/go-redis/v9"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/require"
+    "go.uber.org/mock/gomock"
 )
 
-func init() {
-	_ = os.Setenv("REDIS_HOST", "localhost")
-	_ = os.Setenv("REDIS_PORT", "6379")
-}
-
 func TestCachedClient(t *testing.T) {
-	var (
-		host = os.Getenv("REDIS_HOST")
-		port = os.Getenv("REDIS_PORT")
-	)
+    var (
+        host = os.Getenv("REDIS_HOST")
+        port = os.Getenv("REDIS_PORT")
+    )
 
-	redisClient := redis.NewClient(&redis.Options{Addr: fmt.Sprintf("%s:%s", host, port)})
-	require.NoError(t, redisClient.Ping(context.Background()).Err())
+    redisClient := redis.NewClient(&redis.Options{Addr: fmt.Sprintf("%s:%s", host, port)})
+    require.NoError(t, redisClient.Ping(context.Background()).Err())
 
-	ytbMock := NewYoutubeClientMock(gomock.NewController(t))
-	client := NewCachedClient(redisClient, ytbMock)
+    ytbMock := NewYoutubeClientMock(gomock.NewController(t))
+    client := NewCachedClient(redisClient, ytbMock)
 
-	err := redisClient.FlushDB(context.Background()).Err()
-	require.NoError(t, err)
+    err := redisClient.FlushDB(context.Background()).Err()
+    require.NoError(t, err)
 
-	expectedThumb := &model.Thumbnail{
-		VideoID: "some_id",
-		Payload: []byte("some payload"),
-		Quality: model.MediumQuality,
-	}
+    expectedThumb := &model.Thumbnail{
+        VideoID: "some_id",
+        Payload: []byte("some payload"),
+        Quality: model.MediumQuality,
+    }
 
-	ytbMock.EXPECT().
-		GetThumbnail(gomock.Any(), "some_id", model.MediumQuality).
-		Return(expectedThumb, nil).Times(1)
+    ytbMock.EXPECT().
+        GetThumbnail(gomock.Any(), "some_id", model.MediumQuality).
+        Return(expectedThumb, nil).Times(1)
 
-	for i := 0; i < 2; i++ {
-		actualThumb, err := client.GetThumbnail(context.Background(), "some_id", model.MediumQuality)
-		assert.NoError(t, err)
-		assert.Equal(t, expectedThumb, actualThumb)
-	}
+    for i := 0; i < 2; i++ {
+        actualThumb, err := client.GetThumbnail(context.Background(), "some_id", model.MediumQuality)
+        assert.NoError(t, err)
+        assert.Equal(t, expectedThumb, actualThumb)
+    }
 }
